841d572c5bcf2a789c2f4873c420b47084be8fd8
RobertoRamosCastro <r.ramos.2017@alumnos.urjc.es>
2023-03-04 17:31:18 +0100

    Able to rea the video, the prob was with the path

--- Obj_Counting_2/object_tracking.py
+++ Obj_Counting_2/object_tracking.py
@@ -1,12 +1,15 @@
 import cv2
 import numpy as np
 
-video = cv2.VideoCapture('los_angeles.mp4')
+video = cv2.VideoCapture('C:\\Users\\Roberto\\Documents\\Udemy_Object_Detection\\object-counting\\Obj_Counting_2\\traffic.mp4')
 
-_, frame = video.read()
+while True:
+    _, frame = video.read()
 
-cv2.imshow('video', frame)
+    cv2.imshow('video', frame)
 
-# si se presiona la tecla 'Esc' sale del bucle
-cv2.waitKey(0)
+    # si se presiona la tecla 'Esc' sale del bucle
+    if(cv2.waitKey(30) == 27):
+        break
 
+cv2.destroyAllWindows()
