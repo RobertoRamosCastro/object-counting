f21aea1f90580bfa442325f9cf135594085dd5d7
RobertoRamosCastro <r.ramos.2017@alumnos.urjc.es>
2023-03-04 17:20:48 +0100

    Trying to read the video

--- Obj_Counting_2/object_tracking.py
+++ Obj_Counting_2/object_tracking.py
@@ -1,28 +1,12 @@
 import cv2
 import numpy as np
 
-def main():
+video = cv2.VideoCapture('los_angeles.mp4')
 
-    cap = cv2.VideoCapture('nuevo_video_angeles.mp4')
+_, frame = video.read()
 
-    # Obtiene la resolución actual del video
-    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+cv2.imshow('video', frame)
 
-    print(frame_width, frame_height)
+# si se presiona la tecla 'Esc' sale del bucle
+cv2.waitKey(0)
 
-    '''while True:
-
-        ret, frame = cap.read()
-
-        cv2.imshow('video',frame)
-
-        if (cv2.waitKey(30) == 27):
-                break
-        
-    # Libera los recursos
-    cap.release()
-    cv2.destroyAllWindows()'''
-
-if __name__ == '__main__':
-    main()
--- Obj_Counting_2/prueba_resize.py
+++ Obj_Counting_2/prueba_resize.py
@@ -8,24 +8,24 @@ frame_width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 # Crea un objeto VideoWriter para guardar el nuevo video con la resolución deseada
-out = cv2.VideoWriter('nuevo_video_angeles.mp4', cv2.VideoWriter_fourcc(*'mp4v'),30, (1280, 720))
+#out = cv2.VideoWriter('nuevo_video_angeles.mp4', cv2.VideoWriter_fourcc(*'mp4v'),30, (1280, 720))
 
 while True:
     # Lee cada frame del video
     ret, frame = video.read()
     
-    print(frame.shape)
     # Si no hay más frames, sale del bucle
     if not ret:
         break
     
-    # Redimensiona el frame al nuevo tamaño
+    cv2.imshow('video',frame)
+    '''# Redimensiona el frame al nuevo tamaño
     frame_resized = cv2.resize(frame, (960, 540))
     
     # Escribe el frame redimensionado en el nuevo video
-    out.write(frame_resized)
+    out.write(frame_resized)'''
 
 # Libera los recursos
 video.release()
-out.release()
+#out.release()
 cv2.destroyAllWindows()
